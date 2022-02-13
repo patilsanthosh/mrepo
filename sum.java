adaca16f4a6a0ae6ed64e1ef77dfdc58e49b191e
patilsanthosh <patilsanthosh1.neo@gmail.com>
2022-02-13 22:31:51 +0530

    Santhosh commit

---
+++ sum.java
@@ -0,0 +1,23 @@
+class sum
+{
+	public static void main(String args[])
+	{
+	int a=2;
+	int b=3;
+	int sum=0;
+	int sub=0;
+	int mul=0;
+	int div=0;
+	sum=a+b;
+	sub=a-b;
+	mul=a*b;
+	div=a/b;
+	//System.out.println("sum of a & b")+(5);
+	System.out.println("sum of "+ a +" & " + b +" = " +sum);
+	System.out.println("sub of "+ a +" & " + b +" = " +sub);
+	System.out.println("mul of "+ a +" & " + b +" = " +mul);
+	System.out.println("div of "+ a +" & " + b +" = " +div);
+	}
+}	
+
+//sum of 2 & 3 = 5 
